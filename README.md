533581e435f7b446f1905407fe12f9f9086e20db
Krishna Mohan <krishanmohank974@gmail.com>
2024-11-29 00:56:13 +0530

    started the groq llm api

---
+++ README.md
@@ -0,0 +1,11 @@
+# Codedock
+
+1. Backend
+2. Frontend
+
+Backend : nodejs
+Frontend : React
+
+## Backend
+
+-   
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,27 @@
 require("dotenv").config();
 
-console.log(process.env.GORQ_API_KEY);
+const groqApiKey = process.env.GROQ_API_KEY;
+
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: groqApiKey });
+
+// to create a chat completion
+async function main() {
+  const chatCompletion = await groq.chat.completions.create({
+    messages: [{ role: "user", content: "write code for a todo application in reactjs" }],
+    model: "llama3-8b-8192",
+    temperature: 0, // controls randomness
+    // max_tokens: 1024, // max number of tokens in the response
+    top_p: 1, // controls randomness
+    stream: true, // to stream the response
+    stop: null,
+  });
+
+  // to stream the response
+  for await (const chunk of chatCompletion) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || ""); // to write the response to the console without a new line    
+  }
+}
+
+main();
